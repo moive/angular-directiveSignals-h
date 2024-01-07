a944c31d8df1425ebbe04f405ac02db5fb131a4e
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-06 23:14:48 -0500

    traditional reactive form

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './product-page/product-page.component';
 
 
 @NgModule({
-  declarations: [
-    ProductPageComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule
-  ]
+  declarations: [ProductPageComponent],
+  imports: [CommonModule, ReactiveFormsModule, ProductsRoutingModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}

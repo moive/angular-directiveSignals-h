41e239a58a89658a21c479c306cdfeecc8a12c57
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-06 23:48:43 -0500

    change directive color

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
+
   constructor(private el: ElementRef<HTMLElement>) {
     // console.log('Constructor directive called');
     console.log(el);
@@ -14,5 +21,12 @@ export class CustomLabelDirective implements OnInit {
   }
   ngOnInit(): void {
     console.log('Directive - NgOnInit() called');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+
+    this.htmlElement.nativeElement.style.color = this._color;
   }
 }

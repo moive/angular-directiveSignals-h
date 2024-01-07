38b7db07fc85711114612d659613b28a160b54e8
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-07 00:17:10 -0500

    change msg directive

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -6,10 +7,17 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    // console.log(value);
+    this.setErrorMessage();
   }
 
   constructor(private el: ElementRef<HTMLElement>) {
@@ -27,6 +35,31 @@ export class CustomLabelDirective implements OnInit {
   setStyle(): void {
     if (!this.htmlElement) return;
 
-    this.htmlElement.nativeElement.style.color = this._color;
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    // console.log(errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'This field is required.';
+      return;
+    }
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText = `Min ${current}/${min} characters`;
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'Not a valid email address';
+      return;
+    }
   }
 }

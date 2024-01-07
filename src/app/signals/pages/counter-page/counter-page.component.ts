18239bbb13b9dd075ca5c174ecaa6c41a52637a3
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-01-07 12:12:39 -0500

    counter signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.component.scss']
+  styleUrls: ['./counter-page.component.scss'],
 })
 export class CounterPageComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter());
 
+  increaseBy(value: number) {
+    // this.counter.set(this.counter() + value);
+    this.counter.update((current) => current + value);
+  }
 }
